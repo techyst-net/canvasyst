626d9ad358fb559595199b635f79bb36ae2706c4
Zeshan Shakeel <zeshanshakil0@gmail.com>
2026-09-13 14:15:54 +0500

    fix(font-loader): update font URLs to use CDN for improved loading performance fix(stickers): replace local URLs with CDN links for sticker assets

--- blocksuite/playground/apps/_common/templates/stickers.ts
+++ blocksuite/playground/apps/_common/templates/stickers.ts
@@ -1,11 +1,11 @@
 export default [
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/1.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/1.png',
     type: 'sticker',
     assets: {
       'jnpiF-qH-9NYu1WEx_asdmPfIBVf_Y5AX64yq-Jvbic=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/1.png',
     },
     content: {
       type: 'page',
@@ -61,11 +61,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/2.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/2.png',
     type: 'sticker',
     assets: {
       'JwGgaM_IRE2A9RLtMk8KKbyHAz1rQ3SxnCmNt5CI0Q4=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/2.png',
     },
     content: {
       type: 'page',
@@ -121,11 +121,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/3.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/3.png',
     type: 'sticker',
     assets: {
       '9UO31nxq6_RN2F__dx__Rp4UbhmmLDz1Nfz_WdnKOXI=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/3.png',
     },
     content: {
       type: 'page',
@@ -181,11 +181,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/4.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/4.png',
     type: 'sticker',
     assets: {
       'lbL16jkIuhQw9gHkalgBT2Bih2AyKOq75NJpE5lZQ_g=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/4.png',
     },
     content: {
       type: 'page',
@@ -241,11 +241,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/5.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/5.png',
     type: 'sticker',
     assets: {
       'kwZRDukIBDUV5ixvz5iT3_WExlc4H5hCrDLZxxgQEu8=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/5.png',
     },
     content: {
       type: 'page',
@@ -301,11 +301,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/6.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/6.png',
     type: 'sticker',
     assets: {
       'Kc9ZAVaolBXfSTZu94gJH5OAbwdwtP7TaIKmWBhEeYo=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/6.png',
     },
     content: {
       type: 'page',
@@ -361,11 +361,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/7.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/7.png',
     type: 'sticker',
     assets: {
       'vY51WmaGESNrWFGsZ8NQ5lembXapx9epsP-ZuOugGvk=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/7.png',
     },
     content: {
       type: 'page',
@@ -421,11 +421,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/8.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/8.png',
     type: 'sticker',
     assets: {
       'YuPd_NI22xNMhfm3GHSonetlA9pebkl-z1fsocwM638=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/8.png',
     },
     content: {
       type: 'page',
@@ -481,11 +481,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/9.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/9.png',
     type: 'sticker',
     assets: {
       '6upuFIKSmrBV8pYRoioRM2jJr6W9-dh-zdMTECor4hk=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/9.png',
     },
     content: {
       type: 'page',
@@ -541,11 +541,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/10.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/10.png',
     type: 'sticker',
     assets: {
       'WTCWRS4fVWExlws9Sgfw6mNkM5tUcsUkDta6cITV5as=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/10.png',
     },
     content: {
       type: 'page',
@@ -601,11 +601,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/11.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/11.png',
     type: 'sticker',
     assets: {
       'hrclQO6EpB6VTJER_N_LkR9P_cK-Ckw0S10x6jf2Btk=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/11.png',
     },
     content: {
       type: 'page',
@@ -661,11 +661,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/12.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/12.png',
     type: 'sticker',
     assets: {
       'WWiNfo_E9g7nllIi9BfvNbJHInhwl5vG26LU56hvtJE=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/12.png',
     },
     content: {
       type: 'page',
@@ -721,11 +721,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/13.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/13.png',
     type: 'sticker',
     assets: {
       '4KbnQ1P38nUux0Tf2fdGqc909fVzGz8PJVuvuSVvnzc=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/13.png',
     },
     content: {
       type: 'page',
@@ -781,11 +781,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/14.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/14.png',
     type: 'sticker',
     assets: {
       '9-e8kW6m8ps6c5UAL4gp6na1xtld40LB9D9y-UxDMHM=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/14.png',
     },
     content: {
       type: 'page',
@@ -841,11 +841,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/15.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/15.png',
     type: 'sticker',
     assets: {
       'wda7t68nS-GZ5nFvtizlZGznECDe__F86Kcz5a5FAWk=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/15.png',
     },
     content: {
       type: 'page',
@@ -901,11 +901,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/16.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/16.png',
     type: 'sticker',
     assets: {
       'cQF0iqkB21MBBJtwCUqb48hbsPpiZ3vZK7YtNXKmos0=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/16.png',
     },
     content: {
       type: 'page',
@@ -961,11 +961,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/17.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/17.png',
     type: 'sticker',
     assets: {
       'pmPMoebd5Z2h2cblWB_TxEw4C5VKdVc0r4BDzMu5Jrw=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/17.png',
     },
     content: {
       type: 'page',
@@ -1021,11 +1021,11 @@ export default [
   },
   {
     preview:
-      'https://zeshan.local,quality=80/templates/stickers/18.png',
+      'https://cdn.affine.pro/cdn-cgi/image/width=80,quality=80/templates/stickers/18.png',
     type: 'sticker',
     assets: {
       'KN7FRbU7RrtqrgjWE3YX_vTnb3NfsSWWbosjoc2GvJI=':
-        'https://zeshan.local',
+        'https://cdn.affine.pro/templates/stickers/18.png',
     },
     content: {
       type: 'page',
